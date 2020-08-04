68e39efbe17b8552bbd6286a165421c49c9efbd6
Tianxu <merryzhou97@gmail.com>
2020-08-04 19:24:19 -0400

    first commit

---
+++ src/data/posts.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    username: 'shader01',
+    userImage: 'https://images-na.ssl-images-amazon.com/images/I/51UW1849rJL._AC_SX466_.jpg',
+    postImage:
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/tropical_beach.jpg',
+    likes: 36,
+    hasBeenLiked: false,
+    caption: 'When you"re ready for summer "18 ☀️',
+    filter: 'perpetua',
+  },
+  {
+    username: 'shader02',
+    userImage: 'https://as2.ftcdn.net/jpg/01/25/23/29/500_F_125232915_XQnESjxiX1RYJceuXgjpnq34Ilo4F4ed.jpg',
+    postImage:
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/downtown.jpg',
+    likes: 20,
+    hasBeenLiked: false,
+    caption: 'Views from the six...',
+    filter: 'clarendon',
+  },
+  {
+    username: 'shader03',
+    userImage:
+      'https://previews.123rf.com/images/samcorp/samcorp1610/samcorp161000109/64631117-techno-abstract-background-glitch-art-style-digital-pixel-noise-computer-screen-signal-error-flow-of.jpg',
+    postImage:
+      'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/puppers.jpg',
+    likes: 49,
+    hasBeenLiked: false,
+    caption: 'Current mood 🐶',
+    filter: 'lofi',
+  },
+];
